0f648a6d5ae7253fb2d246520e84b25fd062785d
pcipr <pcipr@BRUTUS>
2026-08-11 10:31:05 +0300

    Add project files.

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PacheteAPP.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
